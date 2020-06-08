d0b65a8f921ac543834551f7ace6d8188383eaab
Vladimir Balko <vbalko@gmail.com>
2020-06-08 20:27:28 +0000

    vuejs exercise components
    Signed-off-by: Vladimir Balko <vbalko@gmail.com>

--- Start/src/App.vue
+++ Start/src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container">
-        <app-header></app-header>       
+        <app-header></app-header>
         <hr>
         <div class="row">
-            <app-servers></app-servers>
+            <servers></servers>
             <app-server-details></app-server-details>
         </div>
         <hr>
@@ -12,17 +12,17 @@
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-    import Footer from './components/Footer.vue';
-    import Servers from './components/Servers.vue';
-    import ServerDetails from './components/ServerDetails.vue';
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
 
     export default {
         components: {
-            'app-header': Header,
-            'app-footer': Footer,
-            'app-servers': Servers,
-            'app-server-details': ServerDetails
+            appHeader: Header,
+            Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
         }
     }
 </script>
